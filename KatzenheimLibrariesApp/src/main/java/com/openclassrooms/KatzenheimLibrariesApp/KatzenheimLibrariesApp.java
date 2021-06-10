1628d6089612301838fd23e23b2f19cd3e950d0b
lorrainefritz <lorraine.fritz0@gmail.com>
2021-06-10 21:47:42 +0200

    modification noms

--- KatzenheimLibrariesApp/src/main/java/com/openclassrooms/KatzenheimLibrariesApp/KatzenheimLibrariesApp.java
+++ KatzenheimLibrariesApp/src/main/java/com/openclassrooms/KatzenheimLibrariesApp/KatzenheimLibrariesApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LesBibliothequesDeKatzenheimApp {
+public class KatzenheimLibrariesApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LesBibliothequesDeKatzenheimApp.class, args);
+		SpringApplication.run(KatzenheimLibrariesApp.class, args);
 	}
 
 }
